fd377d2ebd20a5df8b09f01579183365598d397f
Anthony Clark <anthonyclark@gmail.com>
2021-02-14 08:11:01 -0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Cloning Zsh plugins..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/jessarcher/zsh-artisan.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/artisan
 
