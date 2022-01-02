45c1d4ac8054c50e9a11287cd273004e88042605
Anthony Clark <anthonyclark@gmail.com>
2022-01-01 18:38:59 -0800

    Update fresh and add Mackup

--- fresh.sh
+++ fresh.sh
@@ -10,14 +10,24 @@ fi
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
-brew bundle
+brew bundle --file $DOTFILES/Brewfile
+
+# Install PHP extensions with PECL
+pecl install imagick redis swoole
 
 # Install global Composer packages
 /opt/homebrew/bin/composer global require laravel/installer laravel/valet beyondcode/expose tightenco/takeout squizlabs/php_codesniffer friendsofphp/php-cs-fixer phpstan/phpstan
@@ -25,25 +35,19 @@ brew bundle
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
 
-# Create a Sites directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
-
-# Clone Github repositories
-./clone.sh
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
-
-# Use .gitignore_global
-git config --global core.excludesfile ~/.dotfiles/.gitignore_global
-
 # Configure git
 git config --global user.name "Anthony Clark"
 git config --global user.email anthonyclark@gmail.com
 git config --global pull.rebase true
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos
+# Create a Sites directory
+mkdir $HOME/Sites
+
+# Clone Github repositories
+$DOTFILES/clone.sh
+
+# Symlink the Mackup config file to the home directory
+ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences - we will run this last because this will reload the shell
+source $DOTFILES/.macos
